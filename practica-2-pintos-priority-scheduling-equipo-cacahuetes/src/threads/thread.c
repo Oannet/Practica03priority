957636c16ab2833486fffc49b3d0b5da4058d498
Kevin-Antonio-Carvajal <Kevin-Antonio@ciencias.unam.mx>
2024-03-14 19:22:39 -0600

    2

--- practica-2-pintos-priority-scheduling-equipo-cacahuetes/src/threads/thread.c
+++ practica-2-pintos-priority-scheduling-equipo-cacahuetes/src/threads/thread.c
@@ -228,18 +228,26 @@ thread_block (void)
    be important: if the caller had disabled interrupts itself,
    it may expect that it can atomically unblock a thread and
    update other data. */
-void
-thread_unblock (struct thread *t) 
-{
-  enum intr_level old_level;
 
-  ASSERT (is_thread (t));
+bool thread_compare(const struct list_elem *a, const struct list_elem *b, void *aux UNUSED) {
+    struct thread *t1 = list_entry(a, struct thread, elem);
+    struct thread *t2 = list_entry(b, struct thread, elem);
+    return t1->priority > t2->priority;
+}
 
-  old_level = intr_disable ();
-  ASSERT (t->status == THREAD_BLOCKED);
-  list_push_back (&ready_list, &t->elem);
-  t->status = THREAD_READY;
-  intr_set_level (old_level);
+void thread_unblock(struct thread *t) {
+    enum intr_level old_level;
+
+    ASSERT(is_thread(t));
+
+    old_level = intr_disable();
+    ASSERT(t->status == THREAD_BLOCKED);
+    list_insert_ordered(&ready_list, &t->elem, thread_compare, NULL);
+    t->status = THREAD_READY;
+    if (thread_current() != idle_thread && t->priority > thread_current()->priority) {
+        thread_yield();
+    }
+    intr_set_level(old_level);
 }
 
 /* Returns the name of the running thread. */
@@ -332,10 +340,21 @@ thread_foreach (thread_action_func *func, void *aux)
 }
 
 /* Sets the current thread's priority to NEW_PRIORITY. */
-void
-thread_set_priority (int new_priority) 
-{
-  thread_current ()->priority = new_priority;
+void thread_set_priority(int new_priority) {
+    struct thread *current_thread = thread_current();
+    int old_priority = current_thread->priority;
+    current_thread->priority = new_priority;
+
+    /* Si la nueva prioridad es menor que la vieja, podría necesitarse previsión. */
+    if (new_priority < old_priority) {
+        /* Verifica si hay hilos de mayor prioridad en la ready_list. */
+        if (!list_empty(&ready_list)) {
+            struct thread *t = list_entry(list_front(&ready_list), struct thread, elem);
+            if (t->priority > new_priority) {
+                thread_yield();
+            }
+        }
+    }
 }
 
 /* Returns the current thread's priority. */
